c5b7cf7a148df1e4e2ec71dad42c662ea0142c04
nikhilpanchal <nikhilpanchal@gmail.com>
2019-02-24 17:34:06 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const HtmlWebpackPluginConfig = new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: [
+        './src/js/start.js',
+        './src/styles/app.css'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        HtmlWebpackPluginConfig
+    ]
+};
